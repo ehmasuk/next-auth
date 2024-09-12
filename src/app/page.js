2b012ad6df4eb6e3a291fd99c24c3e4fc9c3c8c6
ehmasuk <ehmasuk@gmail.com>
2024-09-12 18:20:55 +0600

    1

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,16 @@ const getData = async () => {
 };
 
 async function Home() {
-    const data = await getData()
+    // const data = await getData()
 
     return <div>
         <ul className="space-y-2 p-10">
             <p className="font-semibold">All names</p>
-            {
+            {/* {
                 data?.map((e,i)=>{
                     return <li key={i}>{e.name}</li>
                 })
-            }
+            } */}
         </ul>
     </div>;
 }
