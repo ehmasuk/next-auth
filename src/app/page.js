a81d050367140e515d7dde8bd0f9596766220364
ehmasuk <ehmasuk@gmail.com>
2024-09-12 17:05:41 +0600

    r

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 const getData = async () => {
-    const res = await fetch("http://localhost:3000/api/users");
+    const res = await fetch("http://127.0.0.1:3000/api/users");
     return await res.json();
 };
 
