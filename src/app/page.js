f784b344f7aa4618565d968c013cba2900eb3d07
ehmasuk <ehmasuk@gmail.com>
2024-09-12 18:22:45 +0600

    3

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
 const getData = async () => {
-    const res = await fetch("http://127.0.0.1:3000/api/users");
+    const res = await fetch("https://eh-next-auth.vercel.app/");
     return await res.json();
 };
 
 async function Home() {
-    // const data = await getData()
+    const data = await getData()
 
     return <div>
         <ul className="space-y-2 p-10">
             <p className="font-semibold">All names</p>
-            {/* {
+            {
                 data?.map((e,i)=>{
                     return <li key={i}>{e.name}</li>
                 })
-            } */}
+            }
         </ul>
     </div>;
 }
